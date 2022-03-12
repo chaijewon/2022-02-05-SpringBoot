1fdbe4b5cf8ded5fcd8abf1c023ac605545c3a92
Administrator <Administrator@804-t>
2022-03-12 10:09:54 +0900

    111111

---
+++ DataInputProject/src/com/it/main/MainClass.java
@@ -0,0 +1,20 @@
+package com.it.main;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.it.dao.*;
+import java.util.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainClass mc=new MainClass();
+		
+	}
+}
